33204744aaf213c6be6e049dbe8bbc257623a707
El LM <lmcj@lmcj.xyz>
2022-02-22 03:43:12 +0000

    awesome config added just to keep track, using copycats at the moment, .xprofile loads autorandr, left handed mouse

---
+++ .config/awesome/config-lm-wip/theme.lua
@@ -0,0 +1,203 @@
+--[[
+-- LM's trial and error theme
+--]]
+
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+
+local gears = require("gears")
+local lain  = require("lain")
+local awful = require("awful")
+local wibox = require("wibox")
+local dpi   = require("beautiful.xresources").apply_dpi
+
+local math, string, tag, tonumber, type, os = math, string, tag, tonumber, type, os
+local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
+
+local theme = {}
+theme.font          = "monospace 8"
+theme.icon_dir                                  = os.getenv("HOME") .. "/.config/awesome/icons"
+
+-- Colors losely based in the Arc GTK theme
+theme.bg_normal     = "#000000"
+theme.bg_focus      = "#5294e2"
+theme.bg_urgent     = "#ff0000"
+theme.bg_minimize   = "#4b5162"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#ffffff"
+theme.fg_focus      = "#000000"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#ffffff"
+
+theme.useless_gap   = dpi(0)
+theme.border_width  = dpi(1)
+theme.border_normal = "#000000"
+theme.border_focus  = "#5294e2"
+theme.border_marked = "#7c818c"
+
+-- Some icons
+theme.bat000charging                            = theme.icon_dir .. "/bat-000-charging.png"
+theme.bat000                                    = theme.icon_dir .. "/bat-000.png"
+theme.bat020charging                            = theme.icon_dir .. "/bat-020-charging.png"
+theme.bat020                                    = theme.icon_dir .. "/bat-020.png"
+theme.bat040charging                            = theme.icon_dir .. "/bat-040-charging.png"
+theme.bat040                                    = theme.icon_dir .. "/bat-040.png"
+theme.bat060charging                            = theme.icon_dir .. "/bat-060-charging.png"
+theme.bat060                                    = theme.icon_dir .. "/bat-060.png"
+theme.bat080charging                            = theme.icon_dir .. "/bat-080-charging.png"
+theme.bat080                                    = theme.icon_dir .. "/bat-080.png"
+theme.bat100charging                            = theme.icon_dir .. "/bat-100-charging.png"
+theme.bat100                                    = theme.icon_dir .. "/bat-100.png"
+theme.batcharged                                = theme.icon_dir .. "/bat-charged.png"
+theme.volhigh                                   = theme.icon_dir .. "/volume-high.png"
+theme.vollow                                    = theme.icon_dir .. "/volume-low.png"
+theme.volmed                                    = theme.icon_dir .. "/volume-medium.png"
+theme.volmutedblocked                           = theme.icon_dir .. "/volume-muted-blocked.png"
+theme.volmuted                                  = theme.icon_dir .. "/volume-muted.png"
+theme.voloff                                    = theme.icon_dir .. "/volume-off.png"
+
+-- Battery widget
+local baticon = wibox.widget.imagebox(theme.bat000)
+local battooltip = awful.tooltip({
+    objects = { baticon },
+    margin_leftright = dpi(15),
+    margin_topbottom = dpi(12)
+})
+battooltip.wibox.fg = theme.fg_normal
+battooltip.textbox.font = theme.font
+battooltip.timeout = 0
+battooltip:set_shape(function(cr, width, height)
+    gears.shape.infobubble(cr, width, height, corner_radius, arrow_size, width - dpi(35))
+end)
+local bat = lain.widget.bat({
+    settings = function()
+        local index, perc = "bat", tonumber(bat_now.perc) or 0
+
+        if perc <= 7 then
+            index = index .. "000"
+        elseif perc <= 20 then
+            index = index .. "020"
+        elseif perc <= 40 then
+            index = index .. "040"
+        elseif perc <= 60 then
+            index = index .. "060"
+        elseif perc <= 80 then
+            index = index .. "080"
+        elseif perc <= 100 then
+            index = index .. "100"
+        end
+
+        if bat_now.ac_status == 1 then
+            index = index .. "charging"
+        end
+
+        baticon:set_image(theme[index])
+        battooltip:set_markup(string.format("\n%s%%, %s", perc, bat_now.time))
+    end
+})
+
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- tasklist_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- prompt_[fg|bg|fg_cursor|bg_cursor|font]
+-- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+-- Generate taglist squares:
+local taglist_square_size = dpi(4)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+    taglist_square_size, theme.fg_normal
+)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+    taglist_square_size, theme.fg_normal
+)
+
+-- Variables set for theming notifications:
+-- notification_font
+-- notification_[bg|fg]
+-- notification_[width|height|margin]
+-- notification_[border_color|border_width|shape|opacity]
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_height = dpi(15)
+theme.menu_width  = dpi(100)
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Define the image to load
+theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
+
+theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
+
+theme.wallpaper = "/home/lmcj/Imágenes/Wallpapers/fish.png"
+
+-- You can use your own layout icons like this:
+theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
+theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
+theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
+theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
+theme.layout_max = themes_path.."default/layouts/maxw.png"
+theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
+theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
+theme.layout_tile = themes_path.."default/layouts/tilew.png"
+theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
+theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
+theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
+theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
+theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
+theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
+theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(
+    theme.menu_height, theme.bg_focus, theme.fg_focus
+)
+
+-- Define the icon theme for application icons. If not set then the icons
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+theme.icon_theme = nil
+
+return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
