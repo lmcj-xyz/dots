7dfc1e79cc5129b2e130c3e06433ba01b08e17d0
El LM <lmcj@lmcj.xyz>
2022-03-01 23:43:30 +0000

    awesomewm config added

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -23,7 +23,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "xresources/theme.lua")
+beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
 
 -- Init all modules (You can add/remove active modules here)
 require("modules.auto-start")
